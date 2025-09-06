75610b28cef92cd28bd7428181622dbd5169b5b8
Arungunti45 <Arungunti45@gmail.com>
2025-09-06 15:29:02 +0530

    Fix: add vite dev dependency for vercel

--- myTask/vite.config.js
+++ myTask/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-
-  base:"/",
-  build:{
-    outDir:"build"
-  },
   plugins: [react()],
-
-
+  build: {
+    outDir: 'dist'
+  }
 })
